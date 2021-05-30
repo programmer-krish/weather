ca8a13d3bb322dde9f35d9873f466ee12a840d4a
programmer-krish <s18002156@ousl.lk>
2021-05-30 11:12:00 +0530

    Updates

---
+++ precache-manifest.93387eab29b68466ee13466aa2d21fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bbd917072d77cd6c1b5da00b52102d",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "544bd95b80f995f3da34",
+    "url": "/weather/static/css/main.7c1ea462.chunk.css"
+  },
+  {
+    "revision": "2267678c8388581f3f2c",
+    "url": "/weather/static/js/2.e07c0d45.chunk.js"
+  },
+  {
+    "revision": "544bd95b80f995f3da34",
+    "url": "/weather/static/js/main.cebf818f.chunk.js"
+  },
+  {
+    "revision": "a439d785d83b9852b72f",
+    "url": "/weather/static/js/runtime-main.2412ce67.js"
+  },
+  {
+    "revision": "bdbdb0215da20b7b54e26a0ce2e98054",
+    "url": "/weather/static/media/sunset.bdbdb021.jpg"
+  }
+]);
